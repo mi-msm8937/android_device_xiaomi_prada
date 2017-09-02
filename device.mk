0772913cc585cd7636175f74a3aa45023c20fa10
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2017-09-03 01:13:25 +0800

    prada: keylayout: Update headphone button layout
    * As per latest MIUI Global Beta.
    * Thanks to danielstuart14.
    
    Signed-off-by: Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
    Signed-off-by: charansai612 <charansai612@gmail.com>

--- device.mk
+++ device.mk
@@ -21,6 +21,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/atmel-maxtouch.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/atmel-maxtouch.kl \
     $(LOCAL_PATH)/keylayout/ft5x06_720p.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/ft5x06_720p.kl \
     $(LOCAL_PATH)/keylayout/gf3208.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gf3208.kl \
+    $(LOCAL_PATH)/keylayout/msm8952-sku1-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/msm8952-sku1-snd-card_Button_Jack.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl
 
 # Sensors
