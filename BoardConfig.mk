2b2f3b567f676426d3f4a3e1060b833ad89e8598
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-09-05 19:29:18 +0200

    prada: Inherit from common tree
    Change-Id: I9eb2ca59312c55d494cb18557689898d9cdf5f62

---
+++ BoardConfig.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from common msm8937-common
+include device/xiaomi/msm8937-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/prada
+
+# Kernel
+TARGET_KERNEL_CONFIG := mi8937_defconfig
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2018-08-01
+
+# SELinux
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
+# Inherit from the proprietary version
+include vendor/xiaomi/prada/BoardConfigVendor.mk
