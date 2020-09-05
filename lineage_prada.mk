408c816c7cfc994f5bd796ade42dc1bf7687e234
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-09-05 19:29:18 +0200

    prada: Inherit from common tree
    Change-Id: I9eb2ca59312c55d494cb18557689898d9cdf5f62

---
+++ lineage_prada.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from prada device
+$(call inherit-product, device/xiaomi/prada/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := prada
+PRODUCT_NAME := lineage_prada
+BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 4
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="prada-user 6.0.1 MMB29M V10.2.2.0.MCEMIXM release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "Xiaomi/prada/prada:6.0.1/MMB29M/V10.2.2.0.MCEMIXM:user/release-keys"
