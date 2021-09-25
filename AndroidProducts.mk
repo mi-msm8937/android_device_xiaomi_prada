7fc43fb83922496a0b3c3691722af7b1fa7380b4
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-09-25 08:10:03 +0800

    CherishOS: prada: Create cherish makefile

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     lineage_prada-user \
     lineage_prada-userdebug \
     lineage_prada-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/cherish_prada.mk
+
+COMMON_LUNCH_CHOICES += \
+    cherish_prada-user \
+    cherish_prada-userdebug \
+    cherish_prada-eng
