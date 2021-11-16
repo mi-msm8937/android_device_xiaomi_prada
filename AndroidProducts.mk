e9e8671011fb62a352b36ba703b32b584405b5fd
me-cafebabe <me-cafebabe@users.noreply.github.com>
2021-11-16 10:26:09 +0800

    RevengeOS: prada: Create makefile

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -11,3 +11,11 @@ COMMON_LUNCH_CHOICES := \
     lineage_prada-user \
     lineage_prada-userdebug \
     lineage_prada-eng
+
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/revengeos_prada.mk
+
+COMMON_LUNCH_CHOICES += \
+    revengeos_prada-user \
+    revengeos_prada-userdebug \
+    revengeos_prada-eng
